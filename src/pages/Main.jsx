968d03165a8eecb6dee3cf63a05d93dd8a12b70f
SadiKISLAR <sadi22kislar@gmail.com>
2022-12-04 15:34:56 +0300

    search button activeted

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -8,10 +8,11 @@ const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("")
+
   useEffect(() => {
     getMovies(FEATURED_API)
   }, [])
-
 
   const getMovies = (API) => {
     axios
@@ -20,13 +21,19 @@ const Main = () => {
       .catch(err => console.log(err));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    getMovies(SEARCH_API + searchTerm)
+  }
+
   return (
     <>
-      <form className="flex justify-center p-2" >
+      <form className="flex justify-center p-2" onSubmit={handleSubmit} >
         <input
           type="search"
           className="w-80 h-8 rounded-md outline-none border p-1 m-2"
           placeholder="Search a movie..."
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <button className="text-white" type="submit">
           Search
