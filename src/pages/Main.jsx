2c5ca5c75603c1bdca3bbcf34edb1f87d3d23364
SadiKISLAR <sadi22kislar@gmail.com>
2022-12-04 16:53:53 +0300

    spinner added

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import MovieCard from "../components/MovieCard";
+import { AuthContext } from "../context/AuthContextProvider";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
@@ -9,22 +10,34 @@ const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}
 const Main = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("")
+  const [loading, setLoading] = useState(false)
+  const { currentUser } = useContext(AuthContext)
 
   useEffect(() => {
     getMovies(FEATURED_API)
   }, [])
 
   const getMovies = (API) => {
+    setLoading(true);
     axios
       .get(API)
       .then(res => setMovies(res.data.results))
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
+      .finally(() => setLoading(false));
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    getMovies(SEARCH_API + searchTerm)
-  }
+    e.preventDefault();
+    if (searchTerm && currentUser) {
+
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm("")
+    } else if (!currentUser) {
+      alert("please log in to see details")
+    } else {
+      alert("please enter a text")
+    }
+  };
 
   return (
     <>
@@ -34,15 +47,23 @@ const Main = () => {
           className="w-80 h-8 rounded-md outline-none border p-1 m-2"
           placeholder="Search a movie..."
           onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
         />
         <button className="text-white" type="submit">
           Search
         </button>
       </form>
       <div className="flex justify-center flex-wrap">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} {...movie} />
-        ))}
+        {loading ? (
+          <div
+            className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
+            role="status"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </div>) : (
+          movies.map((movie) => (
+            <MovieCard key={movie.id} {...movie} />
+          )))}
       </div>
     </>
   );
