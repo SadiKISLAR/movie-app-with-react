a59242df9bf67b5fd0ec0f5995d359c81dfbc128
SadiKISLAR <sadi22kislar@gmail.com>
2022-12-04 14:55:10 +0300

    movie card done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const MovieCard = () => {
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const defaultImage =
+    "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
+
+const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
     return (
         <div
             className="movie"
